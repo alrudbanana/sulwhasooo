8cd837f4df7871878687abf8e20ffb2124055081
migyeongbanana <pratt3343@gmail.com>
2023-12-05 22:11:47 +0900

    gnb_menu 수정

--- script/gnb.js
+++ script/gnb.js
@@ -1,22 +1,19 @@
 $(function(){
-    const gnb = $('.gnb')// gnb메뉴 
-    const gnb_menu_list = $('.d1_over')
-    const gnb_menu_item_list = $('.d1_over > .nav-d2-box')
-    
-    const sub_menu = $('.d1_over > .nav-d2-box > .nav-d2')
-    const bg = document.querySelector('.bg');
+    const gnb_d1 = $('.d1_over');
+    const navD2Box = $('.nav-d2-box');
+    const bg = $('.bg');
 
-    
+    gnb_d1.mouseenter(function(){
+        $(this).children('.nav-d2-box').stop().fadeIn(300)
+        $(this).children('.nav-d2-box').css({'visibility':'visible'});
+        let navD2BoxHeight = $(this).children('.nav-d2-box').outerHeight(true);
+        bg.css({'height':navD2BoxHeight ,'border-top':'1px solid #ccc'})
+        
+    });
 
-    gnb_menu_list.mouseenter(function(){
-        $(this).children('.nav-d2-box').stop().slideDown();
-        bg.style.height = gnb_menu_item_list.outerHeight(true) + 'px';
-        console.log(gnb_menu_item_list.height())
-    })
-
-    gnb_menu_list.mouseleave(function(){
-        $(this).children('.nav-d2-box').stop().slideUp();
-        bg.style.height = 0;
+    gnb_d1.mouseleave(function(){
+        $(this).children('.nav-d2-box').css({'visibility':'hidden'});
+        bg.css('height', 0)
     });
 
 })
